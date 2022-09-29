dd5b1a2e03b9540f68dd24bb54064e1c70331aba
Akramshaha <akramshaha1617@gmail.com>
2022-09-29 10:16:19 +0530

    Git Repo initialization

---
+++ src/main/java/co/in/nextgencoder/exceltosql/ExceltosqlApplication.java
@@ -0,0 +1,13 @@
+package co.in.nextgencoder.exceltosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceltosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceltosqlApplication.class, args);
+	}
+
+}
